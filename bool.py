19c9bc2f06c1eca459e4419cdb4616b716e6999c
TelbiszKende <telbiszkende@gmail.com>
2025-10-09 10:36:08 +0200

    Add files via upload

---
+++ bool.py
@@ -0,0 +1,12 @@
+szám_1 = 5
+szám_2 = 12
+
+e1 = szám_1 > szám_2
+e2 = szám_1 < szám_2
+e3 = szám_1 == szám_2 #ha egyenlő
+e4 = szám_2 != szám_1 #ha nem egynelő
+print(f"Az e1 eredménye: {e1}")
+print(f"Az e2 eredménye: {e2}")
+print(f"Az e3 eredménye: {e3}")
+print(f"az e4 eredménye: {e4}")
+
