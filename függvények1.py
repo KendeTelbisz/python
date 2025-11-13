4c266c2f531fb40efd6d272507cbc434b3382eb0
TelbiszKende <telbiszkende@gmail.com>
2025-11-13 09:23:46 +0100

    Add files via upload

---
+++ függvények1.py
@@ -0,0 +1,13 @@
+def összeg(a: int, b: int) -> int:
+    return a + b
+
+def összeg2(a: int, b: int) -> int:
+    össz = a + b
+    return össz
+
+def összeg3(a: int, b: int) -> int:
+    print(f"A két szám összege: {a + b}")
+
+print(f"A két szám összege: {összeg(66, 89)}")
+print(f"A két szám összege: {összeg2(66, 89)}")
+összeg3(66, 89)
